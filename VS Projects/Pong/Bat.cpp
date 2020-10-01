2c21c00401423e975130831ff36b1aaea3be4bba
Adriancyk <yikun2@illinois.edu>
2020-09-30 21:51:37 -0500

    Update Pong

--- VS Projects/Pong/Bat.cpp
+++ VS Projects/Pong/Bat.cpp
@@ -1,8 +1,6 @@
 #include "Bat.h"
 
-// This the constructor and it is called when we create an object
-Bat::Bat(float startX, float startY)
-{
+Bat::Bat(float startX, float startY) {
     m_Position.x = startX;
     m_Position.y = startY;
 
@@ -13,28 +11,22 @@ Bat::Bat(float startX, float startY)
 FloatRect Bat::getPosition() {
     return m_Shape.getGlobalBounds();
 }
-RectangleShape Bat::getShape()
-{
+RectangleShape Bat::getShape() {
     return m_Shape;
 }
-void Bat::moveLeft()
-{
+void Bat::moveLeft() {
     m_MovingLeft = true;
 }
-void Bat::moveRight()
-{
+void Bat::moveRight() {
     m_MovingRight = true;
 }
-void Bat::stopLeft()
-{
+void Bat::stopLeft() {
     m_MovingLeft = false;
 }
-void Bat::stopRight()
-{
+void Bat::stopRight() {
     m_MovingRight = false;
 }
-void Bat::update(Time dt)
-{
+void Bat::update(Time dt) {
     if (m_MovingLeft) {
         m_Position.x -= m_Speed * dt.asSeconds();
     }
